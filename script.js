7e1c203459d93af37711fb0719e8065c38308222
kchen1999 <kchen174175@gmail.com>
2022-02-26 23:13:22 +1100

    added while loop if computer selects same object as the player to redo the round till a winner is reached

--- script.js
+++ script.js
@@ -16,43 +16,55 @@ function computerPlay() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toUpperCase();
-    computerSelection = computerSelection.toUpperCase();
-    if(playerSelection === computerSelection) { //in the case of a tie
-        return "Draw";
-    }
-    else if(playerSelection === "ROCK" && computerSelection === "PAPER") {
+    if(playerSelection === "ROCK" && computerSelection === "PAPER") {
         computerScore++; 
+        printScore();
         return "You lose! Paper beats Rock";
     }
     else if(playerSelection === "ROCK" && computerSelection === "SCISSORS") {
         playerScore++;
+        printScore();
         return "You win! Rock beats Scissors";
     }
     else if(playerSelection === "PAPER" && computerSelection === "ROCK") {
         playerScore++;
+        printScore();
         return "You win! Paper beats Rock";
     }
     else if(playerSelection === "PAPER" && computerSelection === "SCISSORS") {
-        computerScore++; 
+        computerScore++;
+        printScore(); 
         return "You lose! Scissors beats Paper";
     }
     else if(playerSelection === "SCISSORS" && computerSelection === "ROCK") {
         computerScore++; 
+        printScore();
         return "You lose! Rock beats Scissors";
     }
     else if(playerSelection === "SCISSORS" && computerSelection === "PAPER") {
         playerScore++;
+        printScore();
         return "You win! Scissors beats Paper";
     }
+}
+
+function printScore() {
+    console.log("Player Score is " + playerScore + " Computer Score is " + computerScore);
 }
 
 
 function game() {
     for (let i = 0; i < 5; i++) {
-        let playerSelection = window.prompt("Enter your selection: (rock/paper/scissors)");
-        let computerSelection = computerPlay(); 
-        console.log(playRound(playerSelection, computerSelection));
+        let playerSelection = window.prompt("Enter your selection: (rock/paper/scissors)").toUpperCase();
+        let computerSelection = computerPlay().toUpperCase(); 
+        while(playerSelection === computerSelection) { //in the case of a tie
+            playerSelection = window.prompt("Enter your selection: (rock/paper/scissors)").toUpperCase();
+            computerSelection = computerPlay().toUpperCase(); 
+            console.log("Draw");
+        }
+        console.log(playRound(playerSelection.toUpperCase(), computerSelection.toUpperCase()));
     }
 }
+
+game();
 

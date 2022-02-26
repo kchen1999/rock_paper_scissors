3e7c6f2bd7f3178c61d31cd592a66eec47d4fa0d
kchen1999 <kchen174175@gmail.com>
2022-02-26 22:14:57 +1100

    added playRound function to return winner/loser/draw

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function computerPlay() {
     let randomNumber = Math.floor(Math.random() * 3 + 1);
     if(randomNumber === 1) {
@@ -8,10 +6,35 @@ function computerPlay() {
     else if(randomNumber === 2){
         return "Paper"; 
     }
-    else {
+    else if(randomNumber === 3) {
         return "Scissors";
     }
 
 }
 
-computerPlay();
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toUpperCase();
+    computerSelection = computerSelection.toUpperCase();
+    if(playerSelection === computerSelection) { //in the case of a tie
+        return "Draw";
+    }
+    else if(playerSelection === "ROCK" && computerSelection === "PAPER") {
+        return "You lose! Paper beats Rock";
+    }
+    else if(playerSelection === "ROCK" && computerSelection === "SCISSORS") {
+        return "You win! Rock beats Scissors";
+    }
+    else if(playerSelection === "PAPER" && computerSelection === "ROCK") {
+        return "You win! Paper beats Rock";
+    }
+    else if(playerSelection === "PAPER" && computerSelection === "SCISSORS") {
+        return "You lose! Scissors beats Paper";
+    }
+    else if(playerSelection === "SCISSORS" && computerSelection === "ROCK") {
+        return "You lose! Rock beats Scissors";
+    }
+    else if(playerSelection === "SCISSORS" && computerSelection === "PAPER") {
+        return "You win! Scissors beats Paper";
+    }
+}
+

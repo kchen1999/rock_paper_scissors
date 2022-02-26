93ab39e21fd4505c71738a9a0da63a9111ae1790
kchen1999 <kchen174175@gmail.com>
2022-02-26 22:26:09 +1100

    added code to print out results of one round

--- script.js
+++ script.js
@@ -38,3 +38,6 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
